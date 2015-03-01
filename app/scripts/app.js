5abc25bf213367914ca30e606acf90b239268f1f
drewdotpro <drew@drew.pro>
2015-03-01 18:15:33 +0000

    Player Controller is Material

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,9 @@
  *
  * Main module of the application.
  */
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
 angular
     .module('drinkingApp', [
         'ngAnimate',
--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -10,36 +10,55 @@
 angular.module('drinkingApp')
     .controller('PlayCtrl', function ($scope, $rootScope, $location, localStorageService) {
         $rootScope.menuActive = "play";
-        
-        if ($rootScope.players == undefined) {            
+        var avatarColours = [
+            "gray",
+            "green",
+            "yellow",
+            "blue",
+            "darkBlue",
+            "deepBlue",
+            "purple",
+            "lightPurple",
+            "red",
+            "pink"
+        ];
+
+        if ($rootScope.players == undefined) {
             // see if we got any people in local storage first             
-            if (localStorageService.get('players') != null) {                
-                $rootScope.players = localStorageService.get('players');                   
+            if (localStorageService.get('players') != null) {
+                $rootScope.players = localStorageService.get('players');
+                //Fix non-title case
+                for (var i = 0; i < $rootScope.players.length; i++) {
+                    $rootScope.players[i].name = $rootScope.players[i].name.toProperCase();
+                }
             } else {
-                $rootScope.players = [];   
+                $rootScope.players = [];
             }
         }
-                        
+
         $scope.player = {
             name: "",
             effects: []
         };
 
         $scope.addPlayer = function () {
-            
+
             if ($scope.player.name.length == 0) {
                 return;
             }
             var alreadyExists = false;
             angular.forEach($rootScope.players, function (aPlayer, key) {
-                if (!alreadyExists && $scope.player.name == aPlayer.name) {
+                if (!alreadyExists && $scope.player.name.toLowerCase() == aPlayer.name.toLowerCase()) {
                     alreadyExists = true;
                 }
             });
             if (alreadyExists) {
                 return alert("That player is already in the list");
             }
-            $rootScope.players.push(angular.copy($scope.player));
+            var newPlayer = angular.copy($scope.player);
+            newPlayer.colour = avatarColours[Math.floor(Math.random()*avatarColours.length)];
+            newPlayer.name = newPlayer.name.toProperCase();
+            $rootScope.players.push(newPlayer);
             $scope.player.name = "";
             $rootScope.gameInProgress = false;
         };
@@ -61,7 +80,8 @@ angular.module('drinkingApp')
             //Clear out any extra in-game data we've got saved against the player, or the game.
             for(var i = 0; i < $rootScope.players.length; i++){
                 $rootScope.players[i] = {
-                    name: $rootScope.players[i].name,
+                    name: $rootScope.players[i].name.toProperCase(),
+                    colour: $rootScope.players[i].colour,
                     effects: []
                 }
             }
@@ -71,10 +91,10 @@ angular.module('drinkingApp')
                 effects: []
             };
             $rootScope.currentPlayerIndex = undefined;
-            
-            // save the players we have here to HTML5 local storage for easy selection next time            
+
+            // save the players we have here to HTML5 local storage for easy selection next time
             localStorageService.set('players', $rootScope.players);
-            
+
             //Go to the game
             return $location.path('/turn');
         }
